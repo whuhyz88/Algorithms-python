4ec95dcdef7a90466e32c9bce535943e824c18fe
whuhyz88 <35840936+whuhyz88@users.noreply.github.com>
2019-10-11 16:13:58 +0800

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -12,6 +12,5 @@ for i in range(len(A)):
     min_idx = i 
     for j in range(i+1, len(A)): 
         if A[min_idx] > A[j]: 
-            min_idx = j 
-                
+            min_idx = j             
     A[i], A[min_idx] = A[min_idx], A[i] 
