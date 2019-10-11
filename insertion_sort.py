836dfe94061f84054d75fa2dc1c0d27e1613a91f
whuhyz88 <35840936+whuhyz88@users.noreply.github.com>
2019-10-11 16:08:34 +0800

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,4 +1,4 @@
-def insertion_sort(A):   #right，n2
+def insertion_sort(A):   #right，O(n)=n**2
     for j in range(1,len(A)):
         key = A[j]
         i = j-1
@@ -7,12 +7,4 @@ def insertion_sort(A):   #right，n2
             A[i] = key
             i = i-1
 
-#另一种
-for i in range(len(A)): 
-    min_idx = i 
-    for j in range(i+1, len(A)): 
-        if A[min_idx] > A[j]: 
-            min_idx = j 
-                
-    A[i], A[min_idx] = A[min_idx], A[i] 
 
